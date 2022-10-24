d0e4c4e272e9bc6d0a7e90c641b1fd871de0d048
Bakirovildar <bakirovildar2011@mail.ru>
2022-10-24 14:22:46 +0500

    add animate for button to down

--- src/shared/HomePages/BannerHome/BannerHome.tsx
+++ src/shared/HomePages/BannerHome/BannerHome.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './bannerhome.scss';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { motion } from 'framer-motion';
 
 export function BannerHome() {
   return (
@@ -9,6 +11,25 @@ export function BannerHome() {
           <div className='bannerText'>
               <div className='h1'>LE STYLIST</div>
               <div className='h2'><span></span>WEAR YOUR DRESS</div>
+              <div className='circle'>
+                  <motion.div
+                      initial={{
+                          y: 5
+                      }}
+                      animate={{
+                          y: 15,
+                          opacity: 0
+                      }}
+                      transition={{
+                          delay: 1,
+                          duration: 1,
+                          repeat: Infinity,
+                          repeatDelay: 0.5
+                      }}
+                  >
+                      <KeyboardArrowDownIcon style={{fontSize: '30px', margin: '5px 0'}}/>
+                  </motion.div>
+              </div>
           </div>
       </div>
   );
