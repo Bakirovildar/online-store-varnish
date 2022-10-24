79f0ef4dc6b9eba6cf09fe8798b4a5935e84576c
Bakirovildar <bakirovildar2011@mail.ru>
2022-10-24 12:47:16 +0500

    add dropdown for header, refactoring ShoppingCartsIcon.tsx (add color)

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,36 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './header.scss'
 import {ShoppingCartsIcon} from "../../Icons/ShoppingCartsIcon";
 import MenuIcon from '@mui/icons-material/Menu';
 
+interface IMenu {
+    id: number,
+    title: string
+}
+
 export function Header() {
-  return (
-      <div className='headerWrapper'>
-          <div className='headerContainer'>
-              <div className='headerLogoWrapper'>
-                  <span>MODIST</span>
-              </div>
+    const [isOpen, setIsOpen] = useState(false)
 
-              <div className='headerNavContainer'>
-                      <div>Home</div>
-                      <div>Shop</div>
-                      <div>About</div>
-                      <div>Blog</div>
-                      <div>Contact</div>
-                      <div className='navShop'>
-                          <ShoppingCartsIcon/>
-                          <span>[0]</span>
-                      </div>
-              </div>
+    const menu: Array<any> = [
+        {id: 1, title: 'Home'},
+        {id: 1, title: 'Shop'},
+        {id: 1, title: 'About'},
+        {id: 1, title: 'Blog'},
+        {id: 1, title: 'Contact'},
+    ]
 
-              <div className='headerMenu'>
-                  <div>
-                      <MenuIcon style={{color: '#6c757d'}}/>
-                      <span>MENU</span>
-                  </div>
-              </div>
+    return (
+        <div className='headerWrapper'>
+            <div className='headerContainer'>
+                <div className='headerLogoWrapper'>
+                    <span>MODIST</span>
+                </div>
 
-          </div>
-      </div>
-  );
+                <div className='headerNavContainer'>
+                    {menu.map(({id, title}: IMenu) => <div key={id}>{title}</div>)}
+                    <div className='navShop'>
+                        <ShoppingCartsIcon color={'white'}/>
+                        <span>[0]</span>
+                    </div>
+                </div>
+
+                <div
+                    onClick={() => setIsOpen(!isOpen)}
+                    className='headerMenu'>
+                    <div>
+                        <MenuIcon style={{color: '#6c757d'}}/>
+                        <span>MENU</span>
+                    </div>
+                </div>
+                {
+                    isOpen
+                        ? <div className='headerMenuLi'>
+                            {menu.map(({id, title}: IMenu) => <div key={id}>{title}</div>)}
+                            <div className='navShop2'>
+                                <ShoppingCartsIcon color={'white'}/>
+                                <span>[0]</span>
+                            </div>
+                        </div>
+                        : ''
+                }
+
+            </div>
+        </div>
+    );
 }
